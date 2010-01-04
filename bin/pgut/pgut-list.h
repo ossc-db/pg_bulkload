31f319b935a23443213299fc8ca7246b2c246e24
mhasegawa <mhasegawa>
2010-01-04 07:41:56 +0000

    Add pgut-be header file.

---
+++ bin/pgut/pgut-list.h
@@ -0,0 +1,20 @@
+/*-------------------------------------------------------------------------
+ *
+ * pgut-list.h
+ *
+ * Copyright (c) 2009, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#ifndef PGUT_LIST_H
+#define PGUT_LIST_H
+
+#include "nodes/pg_list.h"
+
+/*
+ * pgut-list.c : Extended list functions
+ */
+extern void list_walk(List *list, void (*walker)());
+
+#endif /* PGUT_LIST_H */
