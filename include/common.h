ebe1889e39153ee157c0bb0b02caac7422fbf426
mhasegawa <mhasegawa>
2010-10-18 02:46:55 +0000

    Add common include file.

---
+++ include/common.h
@@ -0,0 +1,17 @@
+/*
+ * pg_bulkload: include/common.h
+ *
+ *	  Copyright (c) 2010, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
+ */
+
+/**
+ * @file
+ * @brief Common definition in pg_bulkload.
+ *
+ */
+#ifndef COMMON_H_INCLUDED
+#define COMMON_H_INCLUDED
+
+#define PG_BULKLOAD_VERSION "3.0beta2"
+
+#endif   /* COMMON_H_INCLUDED */
