4ff4719166bd1988f2a2b24e36eb2a4e1a0382b6
oonishitk <oonishitk>
2013-12-10 12:28:20 +0900

    Fix the definition of setNewRelfilenode for PostgreSQL 9.0

--- lib/pgut/pgut-be.h
+++ lib/pgut/pgut-be.h
@@ -169,7 +169,7 @@ extern Datum ExecFetchSlotTupleDatum(TupleTableSlot *slot);
 #if PG_VERSION_NUM < 80300
 #define RelationSetNewRelfilenode(rel, xid) \
 	setNewRelfilenode((rel))
-#elif PG_VERSION_NUM < 90100
+#elif PG_VERSION_NUM < 90000
 #define RelationSetNewRelfilenode(rel, xid) \
 	setNewRelfilenode((rel), (xid))
 #elif PG_VERSION_NUM < 90300
